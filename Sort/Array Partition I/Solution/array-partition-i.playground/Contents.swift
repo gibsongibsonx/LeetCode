344e5231cb5d68cde26be0860bf9c4cfeac8ded6
AsahiOcean <asahiocean@icloud.com>
2021-03-17 00:37:25 +0500

    Update Contents.swift

--- Sort/Array Partition I/Solution/array-partition-i.playground/Contents.swift
+++ Sort/Array Partition I/Solution/array-partition-i.playground/Contents.swift
@@ -3,6 +3,10 @@ import XCTest
 
 // https://leetcode.com/problems/array-partition-i/
 
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn)
+// such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+
 class Solution {
     func arrayPairSum(_ nums: [Int]) -> Int {
         var arr = nums
