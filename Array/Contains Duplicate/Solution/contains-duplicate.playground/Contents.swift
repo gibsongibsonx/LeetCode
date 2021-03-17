4bae9d4161c8d5d8bf5a11a4efbd3b6e13d36827
AsahiOcean <asahiocean@icloud.com>
2021-03-18 00:12:52 +0500

    contains duplicate tests

--- Array/Contains Duplicate/Solution/contains-duplicate.playground/Contents.swift
+++ Array/Contains Duplicate/Solution/contains-duplicate.playground/Contents.swift
@@ -7,3 +7,12 @@ class Solution {
         return nums.count > Set(nums).count
     }
 }
+
+func tests() {
+    let s = Solution()
+    assert(s.containsDuplicate([1,2,3,1]) == true)
+    assert(s.containsDuplicate([1,2,3,4]) == false)
+    assert(s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]) == true)
+}
+
+tests()
