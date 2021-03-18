09714ce4110d8b7d38eeec7096301ba493e5541f
AsahiOcean <asahiocean@icloud.com>
2021-03-18 13:33:04 +0500

    two-sum solution

--- Array/Two Sum/Solution/two-sum.playground/Contents.swift
+++ Array/Two Sum/Solution/two-sum.playground/Contents.swift
@@ -1,7 +1,16 @@
 import Foundation
 
+// https://leetcode.com/problems/two-sum/
+
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        
+        var dict = [Int:Int]()
+        for (i, num) in nums.enumerated() {
+            if let last = dict[target - num] {
+                return [last, i]
+            }
+            dict[num] = i
+        }
+        return []
     }
 }
