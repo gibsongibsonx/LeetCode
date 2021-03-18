e41137f734941995e96f09b11faab3ee8523e6ed
AsahiOcean <asahiocean@icloud.com>
2021-03-18 13:46:18 +0500

    two-sum tests

--- Array/Two Sum/Solution/two-sum.playground/Contents.swift
+++ Array/Two Sum/Solution/two-sum.playground/Contents.swift
@@ -14,3 +14,12 @@ class Solution {
         return []
     }
 }
+
+func tests() {
+    let s = Solution()
+    assert(s.twoSum([2,7,11,15], 9) == [0,1])
+    assert(s.twoSum([3,2,4], 6) == [1,2])
+    assert(s.twoSum([3,3], 6) == [0,1])
+}
+
+tests()
