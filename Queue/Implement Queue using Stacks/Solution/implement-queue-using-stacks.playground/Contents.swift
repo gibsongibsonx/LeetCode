d12b3c87e44368eaccb5424ec9ebd0dc333d71d1
AsahiOcean <asahiocean@icloud.com>
2021-03-19 00:19:19 +0500

    Implement Queue using Stacks: Solution

--- Queue/Implement Queue using Stacks/Solution/implement-queue-using-stacks.playground/Contents.swift
+++ Queue/Implement Queue using Stacks/Solution/implement-queue-using-stacks.playground/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// https://leetcode.com/problems/implement-queue-using-stacks/
+
+class MyQueue {
+    
+    fileprivate var array: [Int]
+    
+    /** Initialize your data structure here. */
+    init() {
+        self.array = [Int]()
+    }
+    
+    /** Push element x to the back of queue. */
+    func push(_ x: Int) {
+        array.append(x)
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    func pop() -> Int {
+        return array.removeFirst()
+    }
+    
+    /** Get the front element. */
+    func peek() -> Int {
+        return array.first ?? 0
+    }
+    
+    /** Returns whether the queue is empty. */
+    func empty() -> Bool {
+        return array.isEmpty
+    }
+}
