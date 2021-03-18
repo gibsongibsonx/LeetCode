ee7c2c6b7fb2980a389a0f197e54fe84e5a7156c
AsahiOcean <asahiocean@icloud.com>
2021-03-18 12:27:21 +0500

    remove-element tests

--- Array/Remove Element/Solution/remove-element.playground/Contents.swift
+++ Array/Remove Element/Solution/remove-element.playground/Contents.swift
@@ -8,3 +8,17 @@ class Solution {
         return nums.count
     }
 }
+
+func tests() {
+    let s = Solution()
+    
+    var test1 = [3,2,2,3]
+    s.removeElement(&test1, 3)
+    assert(test1 == [2,2])
+    
+    var test2 = [0,1,2,2,3,0,4,2]
+    s.removeElement(&test2, 2)
+    assert(test2 == [0,1,3,0,4])
+}
+
+tests()
