57098c38baad5adaad56a977bb2943af01b7e097
AsahiOcean <asahiocean@icloud.com>
2021-03-22 20:19:02 +0500

    Update Contents.swift

--- Sort/Minimum Deletions to Make Character Frequencies Unique/Solution/minimum-deletions-to-make-character-frequencies-unique.playground/Contents.swift
+++ Sort/Minimum Deletions to Make Character Frequencies Unique/Solution/minimum-deletions-to-make-character-frequencies-unique.playground/Contents.swift
@@ -2,6 +2,14 @@ import Foundation
 
 // https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
 
+/*
+A string s is called good if there are no two different characters in s that have the same frequency.
+
+Given a string s, return the minimum number of characters you need to delete to make s good.
+
+The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+*/
+
 class Solution {
     func minDeletions(_ s: String) -> Int {
         guard s.count > 0 else { return 0 }
