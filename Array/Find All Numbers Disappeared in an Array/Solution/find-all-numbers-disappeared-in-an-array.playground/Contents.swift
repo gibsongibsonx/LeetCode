e1a44d9839afc6a57ff7900e16ac3d245390252b
AsahiOcean <asahiocean@icloud.com>
2021-03-22 15:59:42 +0500

    Completed

--- Array/Find All Numbers Disappeared in an Array/Solution/find-all-numbers-disappeared-in-an-array.playground/Contents.swift
+++ Array/Find All Numbers Disappeared in an Array/Solution/find-all-numbers-disappeared-in-an-array.playground/Contents.swift
@@ -1,7 +1,19 @@
-import UIKit
+import Foundation
 
 class Solution {
     func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        var result = [Int]()
         
+        nums.enumerated().forEach({
+            let index = abs(nums[$0.offset]) - 1
+            guard nums[index] > 0 else { return }
+            nums[index] = -nums[index]
+        })
+
+        for i in 0..<nums.count where nums[i] > 0 {
+            result.append(i+1)
+        }
+        return result
     }
 }
