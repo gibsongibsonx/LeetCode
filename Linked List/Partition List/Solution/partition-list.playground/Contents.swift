a1791e2a33b34f3871294fd54b266598a2245e60
AsahiOcean <asahiocean@icloud.com>
2021-03-22 17:43:12 +0500

    Partition List: Solution

--- Linked List/Partition List/Solution/partition-list.playground/Contents.swift
+++ Linked List/Partition List/Solution/partition-list.playground/Contents.swift
@@ -1,17 +1,34 @@
 import Foundation
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init() { self.val = 0; self.next = nil; }
- *     public init(_ val: Int) { self.val = val; self.next = nil; }
- *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
- * }
- */
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
 class Solution {
     func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+        let prevgag = ListNode(0), postgag = ListNode(0)
+        var prev = prevgag, post = postgag
         
+        var node: ListNode? = head
+        
+        while node != nil {
+            let next = node!.next
+            node!.next = nil
+            
+            if node!.val < x {
+                prev.next = node
+                prev = prev.next!
+            } else {
+                post.next = node
+                post = post.next!
+            }
+            node = next
+        }
+        prev.next = postgag.next
+        return prevgag.next
     }
 }
