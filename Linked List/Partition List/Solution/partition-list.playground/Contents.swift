9972c1b4f1629486106b75be1361f0b3adda7b57
AsahiOcean <asahiocean@icloud.com>
2021-03-22 17:19:04 +0500

    Partition List: Start

--- Linked List/Partition List/Solution/partition-list.playground/Contents.swift
+++ Linked List/Partition List/Solution/partition-list.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+        
+    }
+}
