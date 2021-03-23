5958129a95444083d993045ad584784fe4569ad0
AsahiOcean <asahiocean@icloud.com>
2021-03-23 06:12:53 +0500

    Update Contents.swift

--- Array/Sliding Window Maximum/Solution/sliding-window-maximum.playground/Contents.swift
+++ Array/Sliding Window Maximum/Solution/sliding-window-maximum.playground/Contents.swift
@@ -2,6 +2,12 @@ import Foundation
 
 // https://leetcode.com/problems/sliding-window-maximum/
 
+/*
+ You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+ 
+ Return the max sliding window.
+*/
+
 class Solution {
     func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
         guard nums.isEmpty == false else { return [] }
@@ -23,3 +29,14 @@ class Solution {
         return right
     }
 }
+
+func tests() {
+    let s = Solution()
+    assert(s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3) == [3,3,5,5,6,7]) // good
+    assert(s.maxSlidingWindow([1], 1) == [1]) // good
+    assert(s.maxSlidingWindow([1,-1], 1) == [1,-1]) // good
+    assert(s.maxSlidingWindow([9,11], 2) == [11]) // good
+    assert(s.maxSlidingWindow([4,-2], 2) == [4]) // good
+}
+
+tests()
