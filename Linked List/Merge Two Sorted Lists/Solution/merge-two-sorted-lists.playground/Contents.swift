2d09342529b3cdb2cec0fd13fb98fb4b2bb2f3f6
AsahiOcean <asahiocean@icloud.com>
2021-03-29 20:29:50 +0500

    Merge Two Sorted Lists: Tests

--- Linked List/Merge Two Sorted Lists/Solution/merge-two-sorted-lists.playground/Contents.swift
+++ Linked List/Merge Two Sorted Lists/Solution/merge-two-sorted-lists.playground/Contents.swift
@@ -6,6 +6,53 @@ import Foundation
  Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
 */
 
+class Solution {
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        guard let l1 = l1 else { return l2 }
+        guard let l2 = l2 else { return l1 }
+        guard l1.val < l2.val else {
+            l2.next = mergeTwoLists(l1, l2.next)
+            return l2
+        }
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    }
+}
+
+// MARK: - Tests
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let s = Solution()
+    
+    func testExample1() {
+        let l1 = ListNode([1,2,4])
+        let l2 = ListNode([1,3,4])
+        let expected = ListNode([1,1,2,3,4,4])
+        XCTAssertEqual(s.mergeTwoLists(l1, l2), expected) // success
+    }
+    
+    func testExample2() {
+        let l1 = ListNode([])
+        let l2 = ListNode([])
+        let expected = ListNode([])
+        XCTAssertEqual(s.mergeTwoLists(l1, l2), expected) // success
+    }
+    
+    func testExample3() {
+        let l1 = ListNode([])
+        let l2 = ListNode([0])
+        let expected = ListNode([0])
+        XCTAssertEqual(s.mergeTwoLists(l1, l2), expected) // success
+    }
+}
+
+Tests.defaultTestSuite.run()
+
+// MARK: - ListNode Class + Extension
+
 public class ListNode {
     public var val: Int
     public var next: ListNode?
@@ -26,15 +73,8 @@ public class ListNode {
     }
 }
 
-class Solution {
-    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        guard let l1 = l1 else { return l2 }
-        guard let l2 = l2 else { return l1 }
-        guard l1.val < l2.val else {
-            l2.next = mergeTwoLists(l1, l2.next)
-            return l2
-        }
-        l1.next = mergeTwoLists(l1.next, l2)
-        return l1
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs.val == rhs.val && lhs.next == rhs.next
     }
 }
