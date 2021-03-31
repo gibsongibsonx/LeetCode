e6f57debc38b742085a6f1c47a36cfb13b18a5a5
AsahiOcean <asahiocean@icloud.com>
2021-04-01 02:57:26 +0500

    Palindrome Number: Tests

--- Math/Palindrome Number/Solution/palindrome-number.playground/Contents.swift
+++ Math/Palindrome Number/Solution/palindrome-number.playground/Contents.swift
@@ -19,3 +19,39 @@ class Solution {
         return reverse() == x
     }
 }
+
+import XCTest
+
+// Executed 4 tests, with 0 failures (0 unexpected) in 0.005 (0.007) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    
+    func testExample1() {
+        // Input: x = 121
+        // Output: true
+        XCTAssertTrue(s.isPalindrome(121) == true)
+    }
+    
+    func testExample2() {
+        // Input: x = -121
+        // Output: false
+        // Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+        XCTAssertTrue(s.isPalindrome(-121) == false)
+    }
+    
+    func testExample3() {
+        // Input: x = 10
+        // Output: false
+        // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+        XCTAssertTrue(s.isPalindrome(10) == false)
+    }
+    
+    func testExample4() {
+        // Input: x = -101
+        // Output: false
+        XCTAssertTrue(s.isPalindrome(-101) == false)
+    }
+}
+
+Tests.defaultTestSuite.run()
