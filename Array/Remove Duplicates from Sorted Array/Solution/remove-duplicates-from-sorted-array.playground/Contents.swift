23ac6bf25603dd6dee47dd915fea2b40bd741406
AsahiOcean <asahiocean@icloud.com>
2021-04-01 03:14:11 +0500

    Remove Duplicates from Sorted Array: Tests

--- Array/Remove Duplicates from Sorted Array/Solution/remove-duplicates-from-sorted-array.playground/Contents.swift
+++ Array/Remove Duplicates from Sorted Array/Solution/remove-duplicates-from-sorted-array.playground/Contents.swift
@@ -10,3 +10,30 @@ class Solution {
         return nums.count
     }
 }
+
+import XCTest
+
+// Executed 2 tests, with 0 failures (0 unexpected) in 0.094 (0.130) seconds
+
+class Tests: XCTestCase {
+    
+    private let s = Solution()
+    
+    func testExample1() {
+        // Input: nums = [1,1,2]
+        // Output: 2, nums = [1,2]
+        var input = [1,1,2]
+        s.removeDuplicates(&input)
+        XCTAssertEqual(2, [1,2].count)
+    }
+    
+    func testExample2() {
+        // Input: nums = [0,0,1,1,1,2,2,3,3,4]
+        // Output: 5, nums = [0,1,2,3,4]
+        var input = [0,0,1,1,1,2,2,3,3,4]
+        s.removeDuplicates(&input)
+        XCTAssertEqual(5, [0,1,2,3,4].count)
+    }
+}
+
+Tests.defaultTestSuite.run()
