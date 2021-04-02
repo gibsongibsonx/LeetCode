1e95f81602380a639d2410bc049b848c4ba7b72a
AsahiOcean <asahiocean@icloud.com>
2021-04-03 04:49:26 +0500

    ZigZag Conversion: Tests

--- String/ZigZag Conversion/Solution/zigzag-conversion.playground/Contents.swift
+++ String/ZigZag Conversion/Solution/zigzag-conversion.playground/Contents.swift
@@ -20,3 +20,25 @@ class Solution {
         return lines.map { String($0) }.joined()
     }
 }
+
+import XCTest
+
+// Executed 3 tests, with 0 failures (0 unexpected) in 0.005 (0.007) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    
+    func testExample1() {
+        XCTAssertEqual(s.convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR") // success
+    }
+    
+    func testExample2() {
+        XCTAssertEqual(s.convert("PAYPALISHIRING", 4), "PINALSIGYAHRPI") // success
+    }
+    
+    func testExample3() {
+        XCTAssertEqual(s.convert("A", 1), "A") // success
+    }
+}
+
+Tests.defaultTestSuite.run()
