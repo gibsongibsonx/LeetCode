019a82719ec40c0728ff17447f36be4362306ef3
AsahiOcean <asahiocean@icloud.com>
2021-04-05 23:08:27 +0500

    Integer to Roman: Tests

--- Math/Integer to Roman/Solution/integer-to-roman.playground/Contents.swift
+++ Math/Integer to Roman/Solution/integer-to-roman.playground/Contents.swift
@@ -6,7 +6,7 @@ class Solution {
         let numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         
         var result = "", number = num
-
+        
         while number > 0 {
             for (i, d) in decimals.enumerated() {
                 if number - d >= 0 {
@@ -19,3 +19,25 @@ class Solution {
         return result
     }
 }
+
+import XCTest
+
+// Executed 5 tests, with 0 failures (0 unexpected) in 0.033 (0.035) seconds
+
+class Tests: XCTestCase {
+    
+    private let s = Solution()
+    
+    func testExample1() { XCTAssert(s.intToRoman(3) == "III") } // success
+    
+    func testExample2() { XCTAssert(s.intToRoman(4) == "IV") } // success
+    
+    func testExample3() { XCTAssert(s.intToRoman(9) == "IX") } // success
+    
+    func testExample4() { XCTAssert(s.intToRoman(58) == "LVIII") } // success
+    
+    func testExample5() { XCTAssert(s.intToRoman(1994) == "MCMXCIV") } // success
+    
+}
+
+Tests.defaultTestSuite.run()
