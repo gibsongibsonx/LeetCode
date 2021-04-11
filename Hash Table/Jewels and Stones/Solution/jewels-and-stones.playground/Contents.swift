3b12b8fe84eb6804074d8f0066fc7813fe77d64b
AsahiOcean <asahiocean@icloud.com>
2021-04-12 02:00:08 +0500

    Jewels and Stones: Done

--- Hash Table/Jewels and Stones/Solution/jewels-and-stones.playground/Contents.swift
+++ Hash Table/Jewels and Stones/Solution/jewels-and-stones.playground/Contents.swift
@@ -7,3 +7,19 @@ class Solution {
         return stones.filter({ jewels.contains($0) }).count
     }
 }
+
+import XCTest
+
+//     Executed 2 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func testExample1() {
+        XCTAssertEqual(s.numJewelsInStones("aA", "aAAbbbb"), 3) // success
+    }
+    func testExample2() {
+        XCTAssertEqual(s.numJewelsInStones("z", "ZZ"), 0) // success
+    }
+}
+
+Tests.defaultTestSuite.run()
