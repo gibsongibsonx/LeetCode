beca2e08d0275b0747b562b6283b66bd11e00515
AsahiOcean <asahiocean@icloud.com>
2021-04-12 01:42:18 +0500

    Design Parking System: Solution

---
+++ Design/Design Parking System/Solution/design-parking-system.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class ParkingSystem {
+    var big: Int, medium: Int, small: Int
+    
+    init(_ big: Int, _ medium: Int, _ small: Int) {
+        self.big = big
+        self.medium = medium
+        self.small = small
+    }
+    
+    func addCar(_ carType: Int) -> Bool {
+        switch carType {
+        case 1:
+            big -= 1
+            return big >= 0
+        case 2:
+            medium -= 1
+            return medium >= 0
+        case 3:
+            small -= 1
+            return small >= 0
+        default:
+            return false
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * let obj = ParkingSystem(big, medium, small)
+ * let ret_1: Bool = obj.addCar(carType)
+ */
