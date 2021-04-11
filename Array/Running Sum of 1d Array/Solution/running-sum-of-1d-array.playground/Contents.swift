9c18f652a6edb53e34b83603cf62b4fe22762441
AsahiOcean <asahiocean@icloud.com>
2021-04-12 01:01:05 +0500

    Running Sum of 1d Array: Done

--- Array/Running Sum of 1d Array/Solution/running-sum-of-1d-array.playground/Contents.swift
+++ Array/Running Sum of 1d Array/Solution/running-sum-of-1d-array.playground/Contents.swift
@@ -4,6 +4,29 @@ import Foundation
 
 class Solution {
     func runningSum(_ nums: [Int]) -> [Int] {
-        
+        var res = [Int]()
+        for i in 0..<nums.count where !nums.isEmpty {
+            i == 0 ? res.append(nums[i]) : res.append(res[i-1] + nums[i])
+        }
+        return res
     }
 }
+
+import XCTest
+
+// Executed 3 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func testExample1() {
+        XCTAssertEqual(s.runningSum([1,2,3,4]), [1,3,6,10]) // success
+    }
+    func testExample2() {
+        XCTAssertEqual(s.runningSum([1,1,1,1,1]), [1,2,3,4,5]) // success
+    }
+    func testExample3() {
+        XCTAssertEqual(s.runningSum([3,1,2,10,1]), [3,4,6,16,17]) // success
+    }
+}
+
+Tests.defaultTestSuite.run()
