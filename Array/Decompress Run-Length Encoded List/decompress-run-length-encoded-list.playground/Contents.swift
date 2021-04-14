4fc81a1b60987f868e01edd61133d9534bf66427
AsahiOcean <asahiocean@icloud.com>
2021-04-14 10:49:47 +0500

    Decompress Run-Length Encoded List: Solution

--- Array/Decompress Run-Length Encoded List/decompress-run-length-encoded-list.playground/Contents.swift
+++ Array/Decompress Run-Length Encoded List/decompress-run-length-encoded-list.playground/Contents.swift
@@ -2,6 +2,10 @@ import Foundation
 
 class Solution {
     func decompressRLElist(_ nums: [Int]) -> [Int] {
-        
+        var res = [Int]()
+        for i in 0..<(nums.count/2) {
+            for _ in 1...nums[2*i] { res.append(nums[2*i+1]) }
+        }
+        return res
     }
 }
