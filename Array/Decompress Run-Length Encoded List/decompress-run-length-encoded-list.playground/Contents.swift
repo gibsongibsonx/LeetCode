c2fc0d492912131cda6d749924be5970e900a55f
AsahiOcean <asahiocean@icloud.com>
2021-04-14 10:50:21 +0500

    Decompress Run-Length Encoded List: Done

--- Array/Decompress Run-Length Encoded List/decompress-run-length-encoded-list.playground/Contents.swift
+++ Array/Decompress Run-Length Encoded List/decompress-run-length-encoded-list.playground/Contents.swift
@@ -9,3 +9,19 @@ class Solution {
         return res
     }
 }
+
+import XCTest
+
+// Executed 2 tests, with 0 failures (0 unexpected) in 0.007 (0.009) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func testExample1() {
+        XCTAssert(s.decompressRLElist([1,2,3,4]) == [2,4,4,4]) // success
+    }
+    func testExample2() {
+        XCTAssert(s.decompressRLElist([1,1,2,3]) == [1,3,3]) // success
+    }
+}
+
+Tests.defaultTestSuite.run()
