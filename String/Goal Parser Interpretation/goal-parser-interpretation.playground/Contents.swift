c322848a779ab784f0eeba7ec8411a104fbb30bf
AsahiOcean <asahiocean@icloud.com>
2021-04-14 11:09:52 +0500

    Update Contents.swift

--- String/Goal Parser Interpretation/goal-parser-interpretation.playground/Contents.swift
+++ String/Goal Parser Interpretation/goal-parser-interpretation.playground/Contents.swift
@@ -1,7 +1,11 @@
 import Foundation
 
+// https://leetcode.com/problems/goal-parser-interpretation/
+
 class Solution {
     func interpret(_ command: String) -> String {
-        
+        if !(1...100).contains(command.count) { return "" }
+        let o = command.replacingOccurrences(of: "()", with: "o")
+        return o.replacingOccurrences(of: "(al)", with: "al")
     }
 }
