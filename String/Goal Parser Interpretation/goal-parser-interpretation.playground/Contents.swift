ce37ded6521ae15cc0e6d001fde21ff302db5b9f
AsahiOcean <asahiocean@icloud.com>
2021-04-14 11:10:21 +0500

    Update Contents.swift

--- String/Goal Parser Interpretation/goal-parser-interpretation.playground/Contents.swift
+++ String/Goal Parser Interpretation/goal-parser-interpretation.playground/Contents.swift
@@ -9,3 +9,22 @@ class Solution {
         return o.replacingOccurrences(of: "(al)", with: "al")
     }
 }
+
+import XCTest
+
+//      Executed 3 tests, with 0 failures (0 unexpected) in 0.005 (0.035) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func testExample1() {
+        XCTAssertEqual(s.interpret("G()(al)"),"Goal") // success
+    }
+    func testExample2() {
+        XCTAssertEqual(s.interpret("G()()()()(al)"),"Gooooal") // success
+    }
+    func testExample3() {
+        XCTAssertEqual(s.interpret("(al)G(al)()()G"),"alGalooG") // success
+    }
+}
+
+Tests.defaultTestSuite.run()
