8a1c3caa9853cc2aa7d049f4d6d183d487b30ef9
ASAHI OCEAN <asahiocean@icloud.com>
2021-06-03 01:50:37 +0500

    Majority Element Solution

--- Array/Majority Element/majority-element.playground/Contents.swift
+++ Array/Majority Element/majority-element.playground/Contents.swift
@@ -1,3 +1,14 @@
 import Foundation
 
 // https://leetcode.com/problems/majority-element/
+
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        var candidate = 0, vote = 0
+        nums.forEach({
+            if vote == 0 { candidate = $0 }
+            $0 == candidate ? (vote += 1) : (vote -= 1)
+        })
+        return candidate
+    }
+}
