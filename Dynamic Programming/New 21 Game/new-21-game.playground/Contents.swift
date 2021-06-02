a804f3035dc882f7fe2a968b8825aa6c31a5f932
ASAHI OCEAN <asahiocean@icloud.com>
2021-06-03 04:36:45 +0500

    837. New 21 Game

---
+++ Dynamic Programming/New 21 Game/new-21-game.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// https://leetcode.com/problems/new-21-game
+
+class Solution {
+    func new21Game(_ n: Int, _ k: Int, _ maxPts: Int) -> Double {
+        guard k != 0 && n < (k + maxPts) else { return 1.0 }
+        var dp = [Double](repeating: 0, count: n+1)
+        dp[0] = 1.0
+        var result = 0.0, val = 1.0
+        for i in 1...n {
+            dp[i] = val / Double(maxPts)
+            i < k ? (val += dp[i]) : (result += dp[i])
+            if i - maxPts >= 0 { val -= dp[i - maxPts] }
+        }
+        return result
+    }
+}
