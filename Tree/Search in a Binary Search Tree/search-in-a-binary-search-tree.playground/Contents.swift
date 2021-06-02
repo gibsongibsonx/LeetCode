796845906539f8843f6bd213b256359fb71f38a5
ASAHI OCEAN <asahiocean@icloud.com>
2021-06-03 04:05:35 +0500

    Update Contents.swift

--- Tree/Search in a Binary Search Tree/search-in-a-binary-search-tree.playground/Contents.swift
+++ Tree/Search in a Binary Search Tree/search-in-a-binary-search-tree.playground/Contents.swift
@@ -1,17 +1,6 @@
 import Foundation
 
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
-    }
-}
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
 
 class Solution {
     func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
@@ -23,5 +12,65 @@ class Solution {
         } else {
             return searchBST(root.right, val)
         }
+    }
+}
+
+// MARK: - Tests
+
+import XCTest
+
+//      Executed 2 tests, with 0 failures (0 unexpected) in 0.212 (0.214) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func test1() {
+        XCTAssertEqual(s.searchBST(TreeNode([4,2,7,1,3]), 2), TreeNode([2,1,3]))
+    }
+    func test2() {
+        XCTAssertEqual(s.searchBST(TreeNode([4,2,7,1,3]), 5), TreeNode([]))
+    }
+}
+
+Tests.defaultTestSuite.run()
+
+// MARK: - TreeNode
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+    
+    // Additional initializer
+    public init?(_ array: [Int?]) {
+        var values = array
+        guard !values.isEmpty, let head = values.removeFirst() else { return nil }
+        
+        val = head; left = nil; right = nil
+        
+        var queue = [self]
+        while !queue.isEmpty {
+            let node = queue.removeFirst()
+            if !values.isEmpty, let val = values.removeFirst() {
+                node.left = TreeNode(val)
+                queue.append(node.left!)
+            }
+            if !values.isEmpty, let val = values.removeFirst() {
+                node.right = TreeNode(val)
+                queue.append(node.right!)
+            }
+        }
+    }
+}
+
+extension TreeNode: Equatable {
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return lhs.left == rhs.left && lhs.right == rhs.right && lhs.val == rhs.val
     }
 }
