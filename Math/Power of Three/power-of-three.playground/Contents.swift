b7bb6d13e3e19ecc65540ac8e0738ee43525f944
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-04 07:07:30 +0500

    Power of Three

---
+++ Math/Power of Three/power-of-three.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+// https://leetcode.com/problems/power-of-three/
+
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        var num = n
+        if n < 1 { return false }
+        while num % 3 == 0 { num = num / 3 }
+        guard num != 1 else { return true }
+        return false
+    }
+}
+
+import XCTest
+
+// Executed 4 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func test1() {
+        XCTAssertEqual(s.isPowerOfThree(27), true)
+    }
+    func test2() {
+        XCTAssertEqual(s.isPowerOfThree(0), false)
+    }
+    func test3() {
+        XCTAssertEqual(s.isPowerOfThree(9), true)
+    }
+    func test4() {
+        XCTAssertEqual(s.isPowerOfThree(45), false)
+    }
+}
+
+Tests.defaultTestSuite.run()
